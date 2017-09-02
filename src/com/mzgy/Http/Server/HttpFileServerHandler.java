320ec1d96325306a42a8f4a615b759bfe6c583c2
mypc <mengzhongguanyu@163.com>
2017-09-02 15:43:34 +0800

    更新

--- src/com/mzgy/Http/Server/HttpFileServerHandler.java
+++ src/com/mzgy/Http/Server/HttpFileServerHandler.java
@@ -2,11 +2,9 @@ package com.mzgy.Http.Server;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.*;
 import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
 
 import static com.sun.deploy.net.HttpRequest.CONTENT_LENGTH;
 import static com.sun.deploy.net.HttpRequest.CONTENT_TYPE;
@@ -17,12 +15,14 @@ import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
  */
 public class HttpFileServerHandler extends ChannelInboundHandlerAdapter {
     private HttpRequest request;
-    private HttpResponse response;
+    private FullHttpResponse response;
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (msg instanceof HttpRequest){
             request = (HttpRequest) msg;
             String url = request.uri();
+            HttpMethod method = request.method();
+            HttpVersion version = request.protocolVersion();
             }
             if(response!=null)
                 ctx.write(response);
@@ -34,13 +34,14 @@ public class HttpFileServerHandler extends ChannelInboundHandlerAdapter {
             buf.release();
 
             String res = "I am OK";
-            FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1,
-                    HttpResponseStatus.OK, Unpooled.wrappedBuffer(res.getBytes("UTF-8")));
-            response.headers().set(CONTENT_TYPE, "text/plain");
-            response.headers().set(CONTENT_LENGTH,
-                    response.content().readableBytes());
-
-            ctx.write(response);
+            StringBuffer stringBuffer = new StringBuffer();
+            stringBuffer.append(res).append("</br>").append("<table><tr><th>标题</th><th>作者</th></tr><tr><td>1</td><td>2</td></tr></table>");
+            response = new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.OK);
+            response.headers().set(CONTENT_TYPE, "text/html;charset=UTF-8");
+            ByteBuf byteBuf = Unpooled.copiedBuffer(stringBuffer, CharsetUtil.UTF_8);
+            response.content().writeBytes(byteBuf);
+            byteBuf.release();
+            ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
             ctx.flush();
         }
     }
