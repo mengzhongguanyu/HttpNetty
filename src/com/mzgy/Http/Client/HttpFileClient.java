4d9a6b6523398878be9ff4b097a0cf7181f9ddc5
mypc <mengzhongguanyu@163.com>
2017-09-02 17:05:15 +0800

    更新

---
+++ src/com/mzgy/Http/Client/HttpFileClient.java
@@ -0,0 +1,59 @@
+package com.mzgy.Http.Client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.*;
+
+import java.net.URI;
+
+/**
+ * Created by mypc on 2017/9/2.
+ */
+public class HttpFileClient {
+    private void connte(String host, int port) throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+
+
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group).channel(NioSocketChannel.class).option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                            socketChannel.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                            socketChannel.pipeline().addLast("fileServerHandler", new HttpFileClientHandler());
+                        }
+                    });
+            ChannelFuture future = bootstrap.connect(host, port).sync();
+            URI uri = new URI("http://127.0.0.1:8080");
+            String msg = "Are you ok?";
+            DefaultFullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET,
+                    uri.toASCIIString(), Unpooled.wrappedBuffer(msg.getBytes()));
+
+            // 构建http请求
+            request.headers().set(HttpHeaders.Names.HOST, host);
+            request.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+            request.headers().set(HttpHeaders.Names.CONTENT_LENGTH, request.content().readableBytes());
+            request.headers().set("messageType", "normal");
+            request.headers().set("businessType", "testServerState");
+            // 发送http请求
+            future.channel().write(request);
+            future.channel().flush();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        HttpFileClient client = new HttpFileClient();
+        client.connte("127.0.0.1", 8080);
+    }
+}
---
+++ src/com/mzgy/Http/Server/HttpFileServerHandler.java
@@ -0,0 +1,50 @@
+package com.mzgy.Http.Server;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import static com.sun.deploy.net.HttpRequest.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+/**
+ * Created by mypc on 2017/8/26.
+ */
+public class HttpFileServerHandler extends ChannelInboundHandlerAdapter {
+    private HttpRequest request;
+    private FullHttpResponse response;
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if (msg instanceof HttpRequest){
+            request = (HttpRequest) msg;
+            String url = request.uri();
+            HttpMethod method = request.method();
+            HttpVersion version = request.protocolVersion();
+            }
+            if(response!=null)
+                ctx.write(response);
+
+        if (msg instanceof HttpContent){
+            HttpContent content = (HttpContent) msg;
+            ByteBuf buf = content.content();
+            String str = buf.toString(CharsetUtil.UTF_8);
+            System.out.println(buf.toString(CharsetUtil.UTF_8));
+            buf.release();
+
+            String res = "I am OK";
+            StringBuffer stringBuffer = new StringBuffer();
+            stringBuffer.append(res).append("</br>").append("<table><tr><th>标题</th><th>作者</th></tr><tr><td>1</td><td>2</td></tr></table>");
+            response = new DefaultFullHttpResponse(HTTP_1_1, HttpResponseStatus.OK);
+            response.headers().set(CONTENT_TYPE, "text/html;charset=UTF-8");
+            ByteBuf byteBuf = Unpooled.copiedBuffer(stringBuffer, CharsetUtil.UTF_8);
+            response.content().writeBytes(byteBuf);
+            byteBuf.release();
+            ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+            ctx.flush();
+        }
+    }
+}
